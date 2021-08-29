36bac975bc5907c47b4ef9e9322f5a3372309a83
TexHexx <prior_it@mail.ru>
2021-08-29 09:39:03 +0300

    Update

--- BlackJack/CardDeck.h
+++ BlackJack/CardDeck.h
@@ -5,6 +5,10 @@ class CardDeck {
 public:
 	static int const FULL_DECK_SIZE = 52;
 	CardDeck();
+	~CardDeck() { 
+		for(int i=0;i<FULL_DECK_SIZE;i++)
+			delete _deck_cards[i]; 
+	};
 private:
 	Card* _deck_cards[FULL_DECK_SIZE];
 	int _deck_size = FULL_DECK_SIZE;
